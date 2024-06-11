96541656aad0cae159f65c165b1daf21833f9a65
Pavel Nikonorov <4646953+pavelnikonorov@users.noreply.github.com>
2024-06-11 12:06:55 +0000

    Serialize, Deserialize, Debug for imds::Certificates

--- az-cvm-vtpm/az-snp-vtpm/src/imds.rs
+++ az-cvm-vtpm/az-snp-vtpm/src/imds.rs
@@ -2,12 +2,12 @@
 // Licensed under the MIT License.
 
 use crate::HttpError;
-use serde::Deserialize;
+use serde::{Serialize, Deserialize};
 
 const IMDS_CERT_URL: &str = "http://169.254.169.254/metadata/THIM/amd/certification";
 
 /// PEM encoded VCEK certificate and AMD certificate chain.
-#[derive(Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Certificates {
     #[serde(rename = "vcekCert")]
     pub vcek: String,
